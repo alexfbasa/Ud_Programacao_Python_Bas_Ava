edeca8ecc7dc70b3dd44c55fe5929ddd181bcd96
alexandre.basilio <alexandre.basilio@kuehne-nagel.com>
2022-12-30 20:40:20 +0200

    Day 4 - and exercises

--- Day-4/main.py
+++ Day-4/main.py
@@ -1,4 +1,48 @@
-import art
 import random
 
-choice = int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors'))
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+get_user_choice = int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n'))
+choices = [rock, paper, scissors]
+size_choices = len(choices)
+computer_choice = random.randint(0, size_choices - 1)
+print(f"Your bet {choices[get_user_choice]}")
+print(get_user_choice)
+print(f"Computer chose: {choices[computer_choice]}")
+print(computer_choice)
+
+if get_user_choice == 0 and computer_choice == 2:
+    print('You win.')
+elif computer_choice == 0 and get_user_choice == 2:
+    print('You lose.')
+elif get_user_choice > computer_choice:
+    print('You win.')
+elif computer_choice > get_user_choice:
+    print('You lose.')
+elif computer_choice == get_user_choice:
+    print(f"It's a draw.")
