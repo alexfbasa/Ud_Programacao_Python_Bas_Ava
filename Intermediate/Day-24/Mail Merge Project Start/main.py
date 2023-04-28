e9eeadcfed56dfcb1ed8e78a83a6548be18bd034
AlexBasa <alexsimple@gmail.com>
2023-04-28 15:20:05 +0300

    Day-16 - Reviewing

--- Intermediate/Day-24/Mail Merge Project Start/main.py
+++ Intermediate/Day-24/Mail Merge Project Start/main.py
@@ -1,4 +1,3 @@
-# TODO: Create a letter using starting_letter.txt
 # for each name in invited_names.txt
 # Replace the [name] placeholder with the actual name.
 # Save the letters in the folder "ReadyToSend".
@@ -6,14 +5,16 @@
 # Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-PLACEHOLDER = "[name]"
+
 with open("./Input/Names/invited_names.txt") as names:
-    names_list = names.read().split()
-    # names_list = names.readline().split()
+    name_list = names.read()
+    names_ordered = name_list.split()
 
 with open("./Input/Letters/starting_letter.txt") as letter:
-    content_letter = letter.read()
-    for name in names_list:
-        with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode="w") as file:
-            new_content = content_letter.replace(PLACEHOLDER, name)
-            file.write(new_content)
+    letter_content = letter.read()
+
+for name in names_ordered:
+    new_letter = letter_content.replace("[name]", name)
+    with open(f"./Output/ReadyToSend/Letter_for_{name}.txt", mode="w") as file:
+        if file.write(new_letter):
+            print("File created. ")
