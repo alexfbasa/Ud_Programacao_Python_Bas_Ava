086bf653e08bf613f25119714611bc20b502718a
alexandre.basilio <alexandre.basilio@kuehne-nagel.com>
2023-05-15 15:56:22 +0300

    Playing day 26

--- Intermediate/Day-25/playing/main.py
+++ Intermediate/Day-25/playing/main.py
@@ -1,29 +1,34 @@
 import turtle
-from turtle import Screen
-
 import pandas
 
-image = "../blank_states_img.gif"
-screen = Screen()
+screen = turtle.Screen()
+map_image = "blank_states_img.gif"
+screen.addshape(map_image)
 screen.title("U.S. States Game")
-turtle.addshape(image)
-turtle.shape(image)
 
-data_source = pandas.read_csv("../50_states.csv")
+turtle.shape(map_image)
+turtle.penup()
+data = pandas.read_csv("50_states.csv")
+states_list = data.state.to_list()
+guessed_states = []
+game_guesses = len(data.state)
+print(game_guesses)
 
-# def onclick(x, y):
-#     print(f"x {x}, y {y}")
-#
-# screen.onclick(onclick)
-# turtle.mainloop()
-
-user_answer = turtle.textinput("Guess the State?", "What's another state's name?").title()
-# states = data_source[data_source['state'] == 'Alabama']
-# print(user_answer)
-all_states = data_source.state.to_list()
-
-if user_answer in all_states:
-    x_position = data_source.state.user_answer[0]
-    print(x_position)
+while len(guessed_states) < game_guesses:
+    user_answer = turtle.textinput(title=f"{len(guessed_states)}/{len(states_list)}", prompt="What's another state's name?").title()
+    if user_answer == "Exit":
+        missing_states = [state for state in states_list if state not in guessed_states]
+        new_data = pandas.DataFrame(missing_states, columns=["Missing States"])
+        new_data.to_csv("missing_states.csv")
+        break
+    if user_answer in states_list:
+        guessed_states.append(user_answer)
+        state_data = data[data.state == user_answer]
+        x = int(state_data.x)
+        y = int(state_data.y)
+        city_turtle = turtle.Turtle()
+        city_turtle.penup()
+        city_turtle.goto(x, y)
+        city_turtle.write(user_answer)
 
 screen.exitonclick()
