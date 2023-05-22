57f96c1db4484a9024b7899e04b4d1e76c2d4904
alexandre.basilio <alexandre.basilio@kuehne-nagel.com>
2023-05-22 11:44:24 +0300

    Playing day 30

--- Intermediate/Day-30/main.py
+++ Intermediate/Day-30/main.py
@@ -7,10 +7,16 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-word = input("Enter a word: ").upper()
-try:
-    output_list = [phonetic_dict[letter] for letter in word]
-except KeyError:
-    print("Error")
-else:
-    print(output_list)
+
+def switch_to_nato():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Please, type only letters.")
+        switch_to_nato()
+    else:
+        print(output_list)
+
+
+switch_to_nato()
