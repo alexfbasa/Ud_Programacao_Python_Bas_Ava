596a4039bbe65730201600f253a935427b67a5e9
alexandre.basilio <alexandre.basilio@kuehne-nagel.com>
2023-07-24 16:30:54 +0300

    dd

--- Training/Day03/ticker_rollercoaster.py
+++ Training/Day03/ticker_rollercoaster.py
@@ -1,14 +1,23 @@
 print("Welcome to the roller-coaster!!")
 height = int(input("What is your height in cm? "))
-
+bill = 0
 if height >= 120:
     print("You can ride the roller-coaster!! :)")
     age = int(input("What is your age? "))
     if age < 12:
-        print("You pay $5.")
+        bill = 5
+        print(f"Child tickets are ${bill}.")
     elif age <= 18:
-        print("You pay $7.")
+        bill = 7
+        print(f"Youth tickets are ${bill}.")
     else:
-        print("You pay $12.")
+        bill = 12
+        print(f"Adult tickets are ${bill}.")
+    wants_photo = input("Do you want a photo taken? Y or N").lower()
+    if wants_photo == "y":
+        bill += 3
+        print(f"You ticket is ${bill}.")
+    else:
+        print(f"You ticket is ${bill}.")
 else:
     print('Sorry, you have to grow taller before you can ride. ')
