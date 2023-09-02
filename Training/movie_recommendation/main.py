2ca775896ec20cb8675daa88ab74790adf9a4b15
AlexBasa <alexsimple@gmail.com>
2023-09-02 18:18:19 +0300

    Day-34 - Reviewing

--- Training/movie_recommendation/main.py
+++ Training/movie_recommendation/main.py
@@ -3,7 +3,7 @@ from prettytable import PrettyTable
 
 data = pd.read_csv("MoviesTopRated.csv")
 
-columns_to_extract = ['title', 'popularity', 'vote_average']
+columns_to_extract = ['title', 'popularity', 'vote_count']
 
 temp_data = data[columns_to_extract]
 
@@ -12,9 +12,9 @@ table = PrettyTable(field_names=columns_to_extract)
 for _, row in temp_data.iterrows():
     table.add_row(row)
 
-table.sortby = 'vote_average'
+table.sortby = 'vote_count'
 table.reversesort = True
 
 movie_max_vote = data[data.vote_count == data.vote_count.max()]
-print(movie_max_vote.title)
-# print(table)
+# print(movie_max_vote.title)
+print(table)
