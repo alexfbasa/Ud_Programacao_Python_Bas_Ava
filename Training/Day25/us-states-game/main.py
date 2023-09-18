23c224b390c8935a8b525f41bd84dc818451ddb2
Alexandre Basilio <alexandre.basilio@kuehne-nagel.com>
2023-09-18 21:49:45 +0300

    dd

--- Training/Day25/us-states-game/main.py
+++ Training/Day25/us-states-game/main.py
@@ -1,9 +1,11 @@
+import random
 import turtle
 
 import pandas
+turtle_colors = ["red", "orange", "green", "blue", "purple"]
 
 screen = turtle.Screen()
-screen.title("U.S States Game")
+screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
@@ -12,26 +14,25 @@ data = pandas.read_csv("50_states.csv")
 states_list = data['state'].to_list()
 
 guessed_states = []
-game_guesses = len(states_list)
 
 while len(guessed_states) < 50:
-    get_player_answer = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
-                                         prompt="What's another state's name?").title()
-    print(len(guessed_states))
-    if get_player_answer == 'Exit':
+    user_answer = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                   prompt="What's another state's name?").title()
+    if user_answer == 'Exit':
         missing_states = [state for state in states_list if state not in guessed_states]
-        new_dataframe = pandas.DataFrame(missing_states, columns=["Missing States"])
-        new_dataframe.to_csv("test.csv")
+        missed_state_data = pandas.DataFrame(missing_states, columns=["Missing States"])
+        missed_state_data.to_csv('missed_states.cvs')
         break
-    if get_player_answer in states_list:
-        guessed_states.append(get_player_answer)
-        state_data = data[data.state == get_player_answer]
-        x = int(state_data.x)
-        y = int(state_data.y)
-        turtle_city = turtle.Turtle()
-        turtle_city.penup()
-        turtle_city.hideturtle()
-        turtle_city.goto(x, y)
-        turtle_city.write(get_player_answer)
+    if user_answer in states_list:
+        guessed_states.append(user_answer)
+        answer = turtle.Turtle()
+        answer.hideturtle()
+        answer.penup()
+        answer.color(random.choice(turtle_colors))
+        state_data = data[data['state'] == user_answer]
+        x_pos = int(state_data.x)
+        y_pos = int(state_data.y)
+        answer.goto(x_pos, y_pos)
+        answer.write(user_answer)
 
 screen.exitonclick()
