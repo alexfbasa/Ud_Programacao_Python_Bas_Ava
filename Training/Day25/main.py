60f08d7602474155e9f5e2d92366e9b0af92ec79
AlexBasa <alexsimple@gmail.com>
2023-09-21 21:26:16 +0300

    Day-34 - Reviewing

--- Training/Day25/main.py
+++ Training/Day25/main.py
@@ -1,13 +1,33 @@
-from turtle import Screen
+import turtle
+import pandas
 
-screen = Screen()
-screen.setup(800, 600)
+screen = turtle.Screen()
+screen.setup(750, 540)
+screen.title("U.S State's Name")
+image = "blank_states_img.gif"
+turtle.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+states_list = data['state'].tolist()
+
+print()
+
+guessed_states = []
+
+while len(guessed_states) < 50:
+    user_guess_state = screen.textinput(f"{len(guessed_states)}/50", prompt="What's another State's name?").title()
+    if user_guess_state == 'Exit':
+        break
+    if user_guess_state in states_list:
+        guessed_states.append(user_guess_state)
+        x_pos = float(data[data['state'] == user_guess_state]['x'])
+        y_pos = float(data[data['state'] == user_guess_state]['y'])
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto(x_pos, y_pos)
+        t.write(user_guess_state)
 
 
-def show_x_y(x, y):
-    print(x, y)
-
-
-screen.onclick(show_x_y)
-
-screen.mainloop()
+screen.exitonclick()
