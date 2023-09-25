230a46ddedf851c6b3585083f4b30d93e4fecdeb
Alexandre Basilio <alexandre.basilio@kuehne-nagel.com>
2023-09-25 20:39:21 +0300

    dd

--- Training/Day27/main.py
+++ Training/Day27/main.py
@@ -1,21 +1,33 @@
 from tkinter import *
 
+
+def mile_to_km():
+    value = float(miles_entry.get())
+    calc = value * 1.609
+    km_result_label['text'] = calc
+
+
 window = Tk()
-window.title("My first GUI Program")
-window.minsize(width=500, height=300)
+window.title("Mile to Km Converter")
+window.config(padx=20, pady=20)
 
-my_label = Label(text='My label', font=("Arial", 22, 'bold'))
-my_label.pack(side='top')
+miles_entry = Entry()
+miles_entry.grid(column=1, row=0)
+miles_entry.config(width=10)
 
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
 
-def print_message_button():
-    my_label['text'] = "I got clicked"
-    my_label.pack()
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
 
+km_result_label = Label(text='0')
+km_result_label.grid(column=1, row=1)
 
-my_label['text'] = 'New text'
+km_label = Label(text='Km')
+km_label.grid(column=2, row=1)
 
-button = Button(text='Click ME', command=print_message_button)
-button.pack()
+calculate_button = Button(text="Calculate", command=mile_to_km)
+calculate_button.grid(column=1, row=2)
 
 window.mainloop()
